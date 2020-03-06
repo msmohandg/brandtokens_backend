2fe33ed0c92a854e4ccb366b493a8adafc57d719
MSMohan <msmohandg@gmail.com>
2020-03-06 17:56:38 +0530

    test contact form

--- api/contact.js
+++ api/contact.js
@@ -4,10 +4,11 @@ const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 router.post('/', (req, res, next) => {
-    
+
     const mailOptions = {
         from: 'szczepanbentyn@gmail.com',
-        to: 'contact@brandtokens.io',
+        //to: 'contact@brandtokens.io',
+        to: 'msmohandg@gmail.com',
         subject: 'New inquiry!',
         text: 'Hi Team, there is a new enquiry from '+ req.body.name + '. Email: ' + req.body.email + 'company website: ' + req.body.website + 'country: ' + req.body.country + 'Note: ' + req.body.note,
         html: '<p> Hi Team, there is a new enquiry from '+ req.body.name + '. <br> Email: ' + req.body.email + ' <br>  company website:  ' + req.body.website + ' <br> country: ' + req.body.country + ' <br> Note: ' + req.body.note +' </p>'
